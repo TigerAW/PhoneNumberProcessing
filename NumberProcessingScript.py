430753b91e75bc60dbf92b4110d0f248054c0c07
TigerAW <adam.wan@hotmail.com>
2022-12-20 15:36:55 +0000

    code:added string processing

--- NumberProcessingScript.py
+++ NumberProcessingScript.py
@@ -5,10 +5,18 @@ phoneNumberList = phoneNumberData.split("\n")
 print(phoneNumberList)
 for index,x in list(enumerate(phoneNumberList)):
     x = re.sub('[^0-9]','', x)
+    x = "(" + x[:3] + ")" + x[3:] #adds brackets
+    x = x[:5] + " " + x[5:]       #adds space
+    x = x[:-4] + "-" + x[-4:]     #adds hyphen
     phoneNumberList[index] = x
 
-print(phoneNumberList)
+#formatted_numbers = ["(" + number[:3] + ")" + number[3:] for number in phoneNumberList]
 
+#spaced_numbers = [number[:5] + " " + number[5:] for number in formatted_numbers]
+
+#hyphen_numbers = [number[:-4] + "-" + number[-4:] for number in spaced_numbers]
+
+print(phoneNumberList)
 
 
 
