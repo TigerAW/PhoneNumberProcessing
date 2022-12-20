9b43d851262d670c18709030cee37cd6924043d2
TigerAW <adam.wan@hotmail.com>
2022-12-20 15:05:50 +0000

    code:added file processing

--- NumberProcessingScript.py
+++ NumberProcessingScript.py
@@ -1,4 +1,16 @@
+import re
 phoneNumberFile = open('PhoneNumbers.Txt','r')
 phoneNumberData = phoneNumberFile.read()
 phoneNumberList = phoneNumberData.split("\n")
 print(phoneNumberList)
+for index,x in list(enumerate(phoneNumberList)):
+    x = re.sub('[^0-9]','', x)
+    phoneNumberList[index] = x
+
+print(phoneNumberList)
+
+
+
+
+
+
